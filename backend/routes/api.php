cb2b26b38266bde98af5ec1bcd4381fe861b334b
mengkongly <mengkong2009@yahoo.com>
2018-10-18 23:29:09 -0500

    Configure JWT

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,7 +4,7 @@ Route::group([
     'middleware' => 'api',
     'prefix' => 'auth'
 
-], function ($router) {
+], function () {
 
     Route::post('login', 'AuthController@login');
     Route::post('logout', 'AuthController@logout');
